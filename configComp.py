39cefcc72a1ef868e8aa39072a6a7ba84cf1cc59
Greg Kean <gregkean@Gregs-MacBook-Pro.local>
2020-01-24 11:23:52 -0400

    changed J to minMax

--- configComp.py
+++ configComp.py
@@ -4,15 +4,18 @@ def configComp(map):
     configs = [[1,1], [1,2], [1,3,3], [1,2,3], [2,2,2], [2,3,3,3], [3,3,3,3]]
     minCost = -1
     minRed = -1
+    bestConfig = -1
     for i in configs:
-        print(i)
-        (tempCost,tempRed) = J(map, configs)
-        if (minCost == -1 or minRed == -1)
+        (tempCost,tempRed) = minMax(map, configs)
+        if minCost == -1 or minRed == -1:
             minCost = tempCost
             minRed = tempRed
-        elif(tempCost < minCost)
+            bestConfig = i
+        elif tempCost < minCost:
             minCost = tempCost
             minRed = tempRed
-        elif(tempCost == minCost)
-            if(tempRed < minRed)
+            bestConfig = i
+        elif tempCost == minCost:
+            if tempRed < minRed:
                 minRed = tempRed
+                bestConfig = i
