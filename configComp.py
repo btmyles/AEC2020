fa9c840737c56c677d2a245bdc4694d551f78c5f
Greg Kean <gregkean@Gregs-MacBook-Pro.local>
2020-01-24 11:43:02 -0400

    Added return of best config

--- configComp.py
+++ configComp.py
@@ -4,18 +4,18 @@ def configComp(map):
     configs = [[1,1], [1,2], [1,3,3], [1,2,3], [2,2,2], [2,3,3,3], [3,3,3,3]]
     minCost = -1
     minRed = -1
-    bestConfig = -1
+    minArr = []
     for i in configs:
-        (tempCost,tempRed) = minMax(map, configs)
+        (tempCost,tempRed,tempArr[]) = minMax(map, configs)
         if minCost == -1 or minRed == -1:
             minCost = tempCost
             minRed = tempRed
-            bestConfig = i
+            minArr = tempArr
         elif tempCost < minCost:
             minCost = tempCost
             minRed = tempRed
-            bestConfig = i
+            minArr = tempArr
         elif tempCost == minCost:
             if tempRed < minRed:
                 minRed = tempRed
-                bestConfig = i
+                minArr = tempArr
